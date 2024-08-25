f0fdec95cad9c2c02def00e4ee6fdc75081dd9f0
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-25 17:37:11 +0700

    s

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MINE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Map height:");
+            int a = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Map length:");
+            int b = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Enter Map:");
+            char[,] map = new char[a,b];
+
+            for(int height =0; height<a;height++)
+            {
+                string hangngang = Console.ReadLine();
+                char[] cacphantu = hangngang.ToCharArray();
+                for (int length = 0; length < b; length++)
+                {
+                    map[height,length]= cacphantu[length];
+                }
+            }
+            for(int height =0; height<a;height++)
+            {
+                for (int length = 0; length < b; length++)
+                {
+                     int[,] surround =
+                     {
+                        {height - 1, length - 1},
+                        {height - 1, length},
+                        {height - 1, length + 1},
+                        {height, length - 1},
+                        {height, length + 1},
+                        {height + 1, length - 1},
+                        {height + 1, length},
+                        {height + 1, length + 1}
+                     };
+                     if (map[height,length] != '*')
+                     {
+                        map[height,length] = '0';
+                        for (int nearby = 0; nearby < surround.GetLength(0); nearby++)
+                        {
+                            int a0 = surround[nearby, 0];
+                            int b0 = surround[nearby, 1];
+                            bool insideMap = a0 >= 0 && a0 < a && b0 >= 0 && b0 < b;
+                            if (insideMap && map[height,length] == '*')
+                            {
+                                map[height,length]++;
+                            }
+                        }
+                     }
+                }
+            }
+            for (int height = 0; height < a; height++)
+            {
+                Console.WriteLine();
+                for (int length = 0; length < b; length++)
+                {
+                    Console.WriteLine(map[height,length]);
+                }
+            }
+
+
+    
+
+        }
+    }
+}
